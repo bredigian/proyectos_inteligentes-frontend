02a86e86743fe68712129b90e5deb49a66816a26
Gianluca Bredice <bredicegian@gmail.com>
2024-07-12 16:18:32 -0300

    feature: config fonts and color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'],
+      },
+      colors: {
+        pi: {
+          blue: {
+            light: '#65BEFC',
+            normal: '#208EC9',
+          },
+          gray: {
+            'ultra-light': '#F2F2F2',
+            light: '#E5E5E5',
+            'semi-light': 'A6A6A6',
+            normal: '#999999',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
