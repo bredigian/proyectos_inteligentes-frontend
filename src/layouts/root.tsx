e4815d8e490eafe4851c66c949bc3a41d0ba36d1
Gianluca Bredice <bredicegian@gmail.com>
2024-07-23 11:40:44 -0300

    feature: add Vercel Analytics support

--- src/layouts/root.tsx
+++ src/layouts/root.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
 import { Outlet } from 'react-router-dom';
@@ -5,6 +6,7 @@ import { Outlet } from 'react-router-dom';
 export const RootLayout = () => {
   return (
     <>
+      <Analytics />
       <Header />
       <Outlet />
       <Footer />
